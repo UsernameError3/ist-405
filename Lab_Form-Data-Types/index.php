6f008bc49d68b8227effb17cc7e46fcfca59631b
Alex Fleming <alexfleming421@gmail.com>
2019-09-15 22:48:02 -0500

    debug

--- Lab_Form-Data-Types/index.php
+++ Lab_Form-Data-Types/index.php
@@ -27,31 +27,31 @@ Tested:     9/15/19
     <form action="response.php" method="post">
         <div id"data">
             <label>First Name:</label><br>
-            <input type="text" name"First_Name"><br>
+            <input type="text" name="First_Name"><br>
             <label>Last Name:</label><br>
-            <input type="text" name"Last_Name"><br>
+            <input type="text" name="Last_Name"><br>
             <label>Street Address:</label><br>
-            <input type="text" name"Street_Address"><br>
+            <input type="text" name="Street_Address"><br>
             <label>City:</label><br>
-            <input type="text" name"City"><br>
+            <input type="text" name="City"><br>
             <label>State:</label><br>
-            <input type="text" name"State"><br>
+            <input type="text" name="State"><br>
             <label>Zip Code:</label><br>
-            <input type="text" name"Zip_Code"><br>
+            <input type="text" name="Zip_Code"><br>
             <label>Phone Number:</label><br>
-            <input type="text" name"Phone_Number" maxlength="10"><br>
+            <input type="text" name="Phone_Number" maxlength="10"><br>
             <label>Birth Date:</label><br>
-            <input type="text" name"Birth_Date"><br>
+            <input type="text" name="Birth_Date"><br>
             <label>Create a User ID: (Limit: 8-12 Characters)</label><br>
-            <input type="text" name"Create_UserID" minlength="8" maxlength="12"><br>
+            <input type="text" name="Create_UserID" minlength="8" maxlength="12"><br>
             <label>Select a Random Number from 1 to 10:</label><br>
-            <input type="text" name"Random_Number" maxlength=2><br>
+            <input type="text" name="Random_Number" maxlength=2><br>
             <label>How much money is currently in your savings?</label><br>
-            <input type="text" name"Savings_Balance"><br>
+            <input type="text" name="Savings_Balance"><br>
             <label>How much money do you save each month?</label><br>
-            <input type="text" name"Monthly_Savings"><br>
+            <input type="text" name="Monthly_Savings"><br>
             <label>What is your savings goal?</label><br>
-            <input type="text" name"Savings_Goal"><br>
+            <input type="text" name="Savings_Goal"><br>
         </div>
         <div id="buttons">
 			<label> </label><br>
--- Lab_Form-Data-Types/response.php
+++ Lab_Form-Data-Types/response.php
@@ -9,8 +9,109 @@ Term:    	Fall 2019
 Developed:  9/15/19
 Tested:     9/15/19
 ******************************************************************************/
+/*
+$First_Name
+$Last_Name
+$Street_Address
+$City
+$State
+$Zip_Code
+$Phone_Number
+$Birth_Date
+$Create_UserID
+$Random_Number
+$Savings_Balance
+$Monthly_Savings
+$Savings_Goal
+*/
+
 
 // Get the Data from the Form:
+$First_Name = filter_input(INPUT_POST, 'First_Name');
+$Last_Name = filter_input(INPUT_POST, 'Last_Name');
+$Street_Address = filter_input(INPUT_POST, 'Street_Address');
+$City = filter_input(INPUT_POST, 'City');
+$State = filter_input(INPUT_POST, 'State');
+$Zip_Code = filter_input(INPUT_POST, 'Zip_Code');
+$Phone_Number = filter_input(INPUT_POST, 'Phone_Number');
+$Birth_Date = filter_input(INPUT_POST, 'Birth_Date');
+$Create_UserID = filter_input(INPUT_POST, 'Create_UserID');
+$Random_Number = filter_input(INPUT_POST, 'Random_Number');
+$Savings_Balance = filter_input(INPUT_POST, 'Savings_Balance');
+$Monthly_Savings = filter_input(INPUT_POST, 'Monthly_Savings');
+$Savings_Goal = filter_input(INPUT_POST, 'Savings_Goal');
+
+//  Validations
+/*
+if ($First_Name === NULL) {                                     //  Validate $First_Name
+    $error_message = 'First Name cannot be empty.';
+} else if (is_numeric($First_Name)) {
+    $error_message = 'First Name cannot be numerical.';
+} else if ($Last_Name === NULL) {                               // Validate $Last_Name 
+    $error_message = 'Last Name cannot be empty.';
+} else if (is_numeric($Last_Name)) {
+    $error_message = 'Last Name cannot be numerical.';
+} else if ($Street_Address === NULL) {                          // Validate $Street_Address 
+    $error_message = 'Street Address cannot be empty.';
+} else if ($City === NULL) {                                    // Validate $City 
+    $error_message = 'City cannot be empty.';
+} else if (is_numeric($City)) {
+    $error_message = 'City cannot be numerical.';
+} else if ($State === NULL) {                                   // Validate $State 
+    $error_message = 'State cannot be empty.';
+} else if (is_numeric($State)) {
+    $error_message = '$State cannot be numerical.';
+} else if ($Zip_Code === NULL) {                                // Validate $Zip_Code 
+    $error_message = '$Zip_Code cannot be empty.';
+} else if ($Zip_Code < 0) {
+    $error_message = '$Zip_Code cannot be empty.';
+} else if ($Phone_Number === NULL) {                            // Validate $Phone_Number 
+    $error_message = '$Phone_Number cannot be empty.';
+} else if ($Phone_Number < 0) {
+    $error_message = '$Phone_Number cannot be empty.';
+} else if ($Birth_Date === NULL) {                              // Validate $Birth_Date 
+    $error_message = '$Birth_Date cannot be empty.';
+} else if ($Birth_Date < 0) {
+    $error_message = '$Birth_Date cannot be empty.';
+} else if ($Create_UserID === NULL) {                           // Validate $Create_UserID 
+    $error_message = '$Create_UserID cannot be empty.';
+} else if ($Create_UserID < 0) {
+    $error_message = '$Create_UserID cannot be empty.';
+} else if ($Random_Number === NULL) {                           // Validate $Random_Number 
+    $error_message = '$Random_Number cannot be empty.';
+} else if ($Random_Number < 0) {
+    $error_message = '$Random_Number cannot be empty.';
+} else if ($Savings_Balance === NULL) {                         // Validate $Savings_Balance 
+    $error_message = '$Savings_Balance cannot be empty.';
+} else if ($Savings_Balance < 0) {
+    $error_message = '$Savings_Balance cannot be empty.';
+} else if ($Monthly_Savings === NULL) {                         // Validate $Monthly_Savings 
+    $error_message = '$Monthly_Savings cannot be empty.';
+} else if ($Monthly_Savings < 0) {
+    $error_message = '$Monthly_Savings cannot be empty.';
+} else if ($Savings_Goal === NULL) {                            // Validate $Savings_Goal 
+    $error_message = '$Savings_Goal cannot be empty.';
+} else if ($Savings_Goal < 0) {
+    $error_message = '$Savings_Goal cannot be empty.';
+} else {
+    $error_message = '';
+}
+*/ 
+
+// If Error Exists, Go to Index Page
+/*
+
+if ($error_message != '') {
+    include('index.php');
+    exit();
+}
+
+*/
+
+// Run Calculations
+
+
+// Apply Formatting
 
 
 
@@ -28,35 +129,32 @@ Tested:     9/15/19
 <body>
 
 
-
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-<span><?php echo htmlspecialchars($var); ?></span>
-
-
 <label>Full Name:</label>
+<span><?php echo ($First_Name); ?></span>
 <label>Address:</label>
+<span><?php echo ($Last_Name); ?></span>
 <label>City, State, and ZIP:</label>
+<span><?php echo ($Street_Address); ?></span>
 <label>Phone Number:</label>
+<span><?php echo ($City); ?></span>
 <label>Birth Date:</label>
+<span><?php echo ($State); ?></span>
 <label>User ID:</label>
+<span><?php echo ($Zip_Code); ?></span>
 <label>Random Number Message:</label>
+<span><?php echo ($Phone_Number); ?></span>
 <label>Amount you need to reach your goal:</label>
+<span><?php echo ($Birth_Date); ?></span>
 <label>Length of time to reach your goal:</label>
+<span><?php echo ($Create_UserID); ?></span>
 <label>Approximate date to reach your goal:</label>
+<span><?php echo ($Random_Number); ?></span>
 <label>Current Timestamp:</label>
+<span><?php echo ($Savings_Balance); ?></span>
 <label>Current Date:</label>
+<span><?php echo ($Monthly_Savings); ?></span>
 <label>Current Time:</label>
+<span><?php echo ($Savings_Goal); ?></span>
 
 
 </body>
