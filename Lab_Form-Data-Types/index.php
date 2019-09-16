4ae0d290d596f5b5d9cc2e80ac7e13359436b762
Alex Fleming <alexfleming421@gmail.com>
2019-09-15 21:54:27 -0500

    Debugging

--- Lab_Form-Data-Types/index.php
+++ Lab_Form-Data-Types/index.php
@@ -24,7 +24,7 @@ Tested:     9/15/19
 </head>
 
 <body>
-    <form action="response.php" method="post">
+    <form action="/response.php" method="post">
         <div id"data">
             <label>First Name:</label></br>
             <input type="text" name"First_Name"></br>
@@ -39,13 +39,13 @@ Tested:     9/15/19
             <label>Zip Code:</label></br>
             <input type="text" name"Zip_Code"></br>
             <label>Phone Number:</label></br>
-            <input type="number" name"Phone_Number" maxlength="10"></br>
+            <input type="text" name"Phone_Number" maxlength="10"></br>
             <label>Birth Date:</label></br>
-            <input type="date" name"Birth_Date"></br>
+            <input type="text" name"Birth_Date"></br>
             <label>Create a User ID: (Limit: 8-12 Characters)</label></br>
             <input type="text" name"Create_UserID" minlength="8" maxlength="12"></br>
             <label>Select a Random Number from 1 to 10:</label></br>
-            <input type="number" name"Random_Number" maxlength=2></br>
+            <input type="text" name"Random_Number" maxlength=2></br>
             <label>How much money is currently in your savings?</label></br>
             <input type="text" name"Savings_Balance"></br>
             <label>How much money do you save each month?</label></br>
@@ -53,10 +53,10 @@ Tested:     9/15/19
             <label>What is your savings goal?</label></br>
             <input type="text" name"Savings_Goal"></br>
         </div>
-        <div id="buttons">
-            <label>&nbsp;</label>
-            <input type"submit" value="Submit"></br>
-        </div>
+
+		<label>&nbsp;</label>
+		<input type"submit" value="Submit"></br>
+
     </form>
 
 </body>
